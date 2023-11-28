fb5db0417a283900971ff78f389608a02cfbf39e
Vasuslaida <146870699+Vasuslaida@users.noreply.github.com>
2023-11-28 15:09:39 +0530

    Create airthematicoper.c

---
+++ airthematicoper.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+float a,b,c;
+printf("enter two numbers");
+scanf("%f%f",&a,&b);
+c=a+b;
+printf("sum=%f\n",c);
+c=a-b;
+printf("difference=%f\n",c);
+c=a*b;
+printf("product=%f\n",c);
+c=a/b;
+printf("divide=%f\n",c);
+}
