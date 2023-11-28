05df728603d72c3b90ec7d604350ef1d5a58d52f
Vasuslaida <146870699+Vasuslaida@users.noreply.github.com>
2023-11-28 15:52:35 +0530

    Create perimeter.c

---
+++ perimeter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+    int l=44,b=56,area;
+    float r;
+    area=l*b;
+    printf("the area of rect=%d\n",area);
+    r=sqrt(area/3.14);
+    float peri;
+    peri=3.14*2*r;
+    printf("perimeter of circle=%f",peri);
+}
