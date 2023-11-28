5c90c33d668d9355dd624752e1bdf559cd430dd3
Vasuslaida <146870699+Vasuslaida@users.noreply.github.com>
2023-11-28 15:20:37 +0530

    Create ifelsecharacter.c

---
+++ ifelsecharacter.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char t;
+    printf("enter the char");
+    scanf("%c",&t);
+if (t=='a'||t=='e'||t=='i'||t=='o'||t=='u'||t=='A'||t=='E'||t=='I'||t=='O'||t== 'U')
+{
+    printf("character is vowel");
+}
+else 
+{
+    printf("character is consonent");
+}
+} 
