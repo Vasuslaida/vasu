9685af2ce8d312cda8f5e5f1bfc96a85ea2e1d12
Vasuslaida <146870699+Vasuslaida@users.noreply.github.com>
2023-11-28 15:19:44 +0530

    Create ifelse.c

---
+++ ifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    char c;
+    printf("enter the character");
+    scanf("%d",&c);
+    if(c%2==0)
+    {
+        printf("character we add is even");
+    }
+
+        else
+{   
+        printf("character is odd");
+}
+    }
